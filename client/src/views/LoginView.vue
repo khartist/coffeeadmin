70d803ac02021a7e078692abe240bb564f07a77a
khartist <tnkhanh2909@gmail.com>
2023-12-04 16:09:47 +0700

    login

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -11,18 +11,40 @@
           <h1 class = "bg-gray-200 p-4 text-center text-red-900">Nice to meet you, again</h1>
           <div class="form_input_body">
             <div class="grid place-items:center">
-              <input class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="text" placeholder="Email" value = "sManager" name = "uName" required />
+              <input v-model = "uName" class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="text" placeholder="Email" name = "uName" required />
               <i class="bx bx-user-pin"></i>
             </div>
             <div class="grid place-items:center">
-              <input class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="password" placeholder="Password" value = "sManager" name = "pwd" required />
+              <input v-model = "pwd" class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="password" placeholder="Password" name = "pwd" required />
               <i class="bx bx-lock"></i>
             </div>
             <div class="grid place-items:center">
-                <button type="submit" class="rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500">Login</button>
+                <button @click = "verify" type="button" class="rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500">Login</button>
             </div>
           </div>
         </form>
       </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                uName: "",
+                pwd: ""
+            }
+        },
+        methods: {
+            verify() {
+                alert(uName+" asjkdbaskjdaskjdak" + pwd)
+                if (this.uName ==="sManager" && this.pwd ==="sManager") {
+                    //this.$router.push({name: 'home'});
+                    alert("success!");
+                } else {
+                    alert('Wrong username or password');
+                }
+            }
+        }
+    }
+</script>
