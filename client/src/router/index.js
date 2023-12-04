36d34512fd69f1e8c32b659ce9f3785a396ea096
khartist <tnkhanh2909@gmail.com>
2023-12-04 13:12:54 +0700

    add route login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            name:'login',
+            component: LoginView
+        },
+        {
             path:'/',
             name:'home',
             component: HomeView
