db0b382d1af9eb4f466ef316e73639a23b1e6cea
khartist <tnkhanh2909@gmail.com>
2023-12-04 15:05:40 +0700

    login view modify

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class="navicon">
-      <div class="title">
-        <a href="">Đắng Coffee</a>
+    <div class="">
+      <div class="bg-gray-200 p-4 grid place-items: center">
+        <a class="text-red-900 text-center">Đắng Coffee</a>
       </div>
     </div>
 
-    <div class="wrapper">
-      <div class="formWelcome">
+    <div class="grid place-items:center bg-gray-200">
+      <div class="">
         <form>
-          <h1>Nice to meet you, again</h1>
+          <h1 class = "bg-gray-200 p-4 text-center text-red-900">Nice to meet you, again</h1>
           <div class="form_input_body">
-            <div class="input-box">
-              <input type="text" placeholder="Email" value = "sManager" name = "uName" required />
+            <div class="grid place-items:center">
+              <input class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="text" placeholder="Email" value = "sManager" name = "uName" required />
               <i class="bx bx-user-pin"></i>
             </div>
-            <div class="input-box">
-              <input type="password" placeholder="Password" value = "sManager" name = "pwd" required />
+            <div class="grid place-items:center">
+              <input class = "rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500" type="password" placeholder="Password" value = "sManager" name = "pwd" required />
               <i class="bx bx-lock"></i>
             </div>
-            <button type="submit" class="btn">Login</button>
+            <div class="grid place-items:center">
+                <button type="submit" class="rounded-md my-1 py-2 px-4 text-red-900 bg-blue-500">Login</button>
+            </div>
           </div>
         </form>
       </div>
