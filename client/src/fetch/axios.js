e1e9d40c8dd42738f2456a6b0a22ee77e188533f
khartist <tnkhanh2909@gmail.com>
2023-12-04 13:19:37 +0700

    restrucre

---
+++ client/src/fetch/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'http://127.0.0.1:3999',
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+
+export default instance
