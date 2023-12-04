8da87bcc9fb2bbd2158a20554ab46c73a7435cce
mwan <minhquang180203@gmail.com>
2023-12-04 21:25:06 +0700

    update

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 console.log("start send request to server");
 const express = require('express')
 const bodyParser = require('body-parser')
+const mysql = require('mysql')
 const cors = require('cors')
 const morgan = require('morgan')
 const {sequelize} = require('./src/models')
 const config = require('./src/config/db.config.js')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'sManager',
+  password: 'sManager',
+  database: 'INTERNET_CAFE'
+})
 
 const app = express();
 app.use(morgan("combined"));
@@ -17,8 +25,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+connection.connect()
+
 // set port, listen for requests
-sequelize.sync({ force: false })
+/*sequelize.sync({ force: false })
 	.then(() => {
 		app.listen(db.config.port, () => console.log(`Express server running on port ${db.config.port}`));
-	})
+	})*/
