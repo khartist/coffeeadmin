64478c70f7a56ee4d98bffe5b82baf40b92aa73b
khartist <tnkhanh2909@gmail.com>
2023-12-04 13:01:54 +0700

    add login view

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="navicon">
+      <div class="title">
+        <a href="">Đắng Coffee</a>
+      </div>
+    </div>
+
+    <div class="wrapper">
+      <div class="formWelcome">
+        <form>
+          <h1>Nice to meet you, again</h1>
+          <div class="form_input_body">
+            <div class="input-box">
+              <input type="text" placeholder="Email" value = "sManager" name = "uName" required />
+              <i class="bx bx-user-pin"></i>
+            </div>
+            <div class="input-box">
+              <input type="password" placeholder="Password" value = "sManager" name = "pwd" required />
+              <i class="bx bx-lock"></i>
+            </div>
+            <button type="submit" class="btn">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+</template>
